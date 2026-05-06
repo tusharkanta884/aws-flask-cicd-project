837a023c8db46dab37bcf92492583415e7d7903d
tusharkanta884 <tusharmahakud88@gmail.com>
2026-05-06 18:34:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Pipeline Working Successfully!"
+    return "CI/CD Webhook Working Successfully!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
